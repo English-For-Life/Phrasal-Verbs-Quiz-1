350661002eb8e0357852d61061908b2728af4d9f
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-27 22:39:24 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Don't expect someone to pick it ___ for you.",
+        options:["up", "over", "out", "for", "after"],
+        answer: 0
+    },
+    {
+     q:"Can you check this information ___ ? I'm not sure if it's true.",
+        options:["over", "out", "up", "after", "for"],
+        answer: 1   
+    },
+    {
+        q:"I've been looking ___ you everywhere.",
+        options:["after", "over", "for", "up", "out"],
+        answer: 2
+    },
+    {
+        q:"Do you have any experience in looking ___ small children?",
+        options:["out", "for", "up", "after", "over"],
+        answer: 3
+    },
+    {
+        q:"He lost his balance and fell ___ .",
+        options:["up", "out", "for", "after", "over"],
+        answer: 4
+    },
+    {
+        q:"Clean ___ after your dog.",
+        options:["up", "out", "down", "away", "for"],
+        answer: 0
+    },
+    {
+        q:"To find ___ more, visit our website. ",
+        options:["out", "down", "up", "for", "away"],
+        answer: 0
+    },
+    {
+        q:"I didn't ask ___ your help.",
+        options:["out", "for", "away", "down", "up"],
+        answer: 1
+    },
+    {
+        q:"She threw ___ her old clothes. ",
+        options:["down", "up", "away", "for", "out"],
+        answer: 2
+    },
+    {
+        q:"Everybody, sit ___ , please!",
+        options:["away", "for", "out", "down", "up"],
+        answer: 3
+    },
+    {
+        q:"I give ___ , I can't do this.",
+        options:["down", "out", "about", "at", "up"],
+        answer: 4
+    },
+    {
+        q:"Stop talking ___ your boyfriend.",
+        options:["about", "at", "out", "down", "up"],
+        answer: 0
+    },
+    {
+        q:"Look ___ the picture and describe it.",
+        options:["out", "at", "down", "up", "about"],
+        answer: 1
+    },
+    {
+        q:"Can I write ___ my address and phone number for you?",
+        options:["at", "up", "down", "about", "out"],
+        answer: 2
+    },
+    {
+        q:"I like to hang ___ with you.",
+        options:["up", "about", "down", "out", "at"],
+        answer: 3
+    }
+]
